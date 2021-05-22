72f82a796bfff4323a10904dbe56a4e7f40564a3
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-05-22 19:13:56 -0300

    Update index.js

--- Assets/src/pages/Detail/index.js
+++ Assets/src/pages/Detail/index.js
@@ -13,7 +13,6 @@ export default function Detail({ navigation }) {
 
         })
 
-<<<<<<< HEAD
      return (
 
        <> 
@@ -23,24 +22,11 @@ export default function Detail({ navigation }) {
                 name={'Jordan 14 Retro Gym Red Toro'}
                 discrp={'Jordan Brand lançou um novo colorway temático Chicago Bulls com o Jordan 14 Retro Gym Red Toro. Este lançamento se parece muito com o Jordan 14 Challenge Red, mas em vez de detalhes amarelos para imitar sua inspiração da Ferrari, o Gym Red 14 implementa painéis brancos para criar a vibração do uniforme dos Bulls.'} 
                 brand={require('../Nike/brand/JordanW.png')}
-                >
-                     
+                >                 
             </Body>
 
        </> 
 
-=======
-    return (
-
-
-        <Body
-        img={require('../Nike/Jordan1.png')}
-        cost={'659,90'}
-        name={'Jordan 14 Retro Gym Red Toro'}
-        discrp={'Jordan Brand lançou um novo colorway temático Chicago Bulls com o Jordan 14 Retro Gym Red Toro. Este lançamento se parece muito com o Jordan 14 Challenge Red, mas em vez de detalhes amarelos para imitar sua inspiração da Ferrari, o Gym Red 14 implementa painéis brancos para criar a vibração do uniforme dos Bulls.'} 
-       > 
-       </Body>
->>>>>>> 9c43103a7c05a0595eb054f6a3f188d26f025e5a
 
      
        
