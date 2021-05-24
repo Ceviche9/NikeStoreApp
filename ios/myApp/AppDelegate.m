78cdf7d252b28910f6cbbc680e65b0dc04839638
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-05-24 20:17:31 -0300

    Update4.7

--- ios/myApp/AppDelegate.m
+++ ios/myApp/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"meuAplicativo"
+                                                   moduleName:@"myApp"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- ios/myAppTests/myAppTests.m
+++ ios/myAppTests/myAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface meuAplicativoTests : XCTestCase
+@interface myAppTests : XCTestCase
 
 @end
 
-@implementation meuAplicativoTests
+@implementation myAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
